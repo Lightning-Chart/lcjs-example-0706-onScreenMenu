ddd431fb6b5ffd10bf9fe4a58bd18533995760ff
snekw <dev.snekw@gmail.com>
2021-09-22 14:01:57 +0300

    Update LCJS version

---
+++ src/index.js
@@ -0,0 +1,187 @@
+/*
+ * LightningChartJS example for rendering a 'Mesh Circle'.
+ */
+// Import LightningChartJS
+const lcjs = require('@arction/lcjs')
+
+// Extract required parts from LightningChartJS.
+const {
+    lightningChart,
+    OnScreenMenuButtonType,
+    OnScreenMenuButtonShape,
+    Themes,
+    ColorRGBA,
+    ColorHEX,
+    SolidFill,
+    PointShape,
+    SolidLine
+} = lcjs
+
+const {
+    createProgressiveTraceGenerator,
+    createProgressiveRandomGenerator 
+} = require('@arction/xydata')
+
+
+const dashboard = lightningChart().Dashboard({
+    numberOfColumns: 1,
+    numberOfRows: 3,
+    // theme: Themes.darkGold
+})
+
+// Add XY Chart to top Cell in Dashboard.
+const chart = dashboard.createChartXY({
+    columnIndex: 0,
+    columnSpan: 1,
+    rowIndex: 0,
+    rowSpan: 2,
+})
+
+// Add Zoom Band Chart to bottom Cell in Dashboard.
+const zoomBandChart = dashboard.createZoomBandChart({
+    columnIndex: 0,
+    columnSpan: 1,
+    rowIndex: 2,
+    rowSpan: 1,
+    axis: chart.getDefaultAxisX(),
+})
+
+// Add LineSeries to the XY Chart.
+const line = chart.addLineSeries().setMouseInteractions(false)
+
+// Styles for 2nd LineSeries
+const customStrokeStyle = new SolidLine({ fillStyle: new SolidFill({ color: ColorRGBA(200, 50, 50) }), thickness: 2 })
+
+// Add 2nd LineSeries to the XY Chart.
+const line2 = chart.addLineSeries().setStrokeStyle(customStrokeStyle).setMouseInteractions(false)
+
+// Add PointSeries to the XY Chart.
+const point = chart
+    .addPointSeries({ pointShape: PointShape.Circle })
+    .setPointFillStyle(new SolidFill({ color:  ColorRGBA(180, 180, 180) }))
+    .setPointSize(10)
+
+
+// Array that keeps data of 1st line series
+const arr1 = []
+// Flag that dispose or restore PointSeries
+let checked = false
+// Data of point coordinates
+let prevPoint
+
+// Fill the Line Series with arbitrary data.
+createProgressiveTraceGenerator()
+    .setNumberOfPoints(250)
+    .generate()
+    .toPromise()
+    .then((data) => {
+        arr1.push(...data)
+        line.add(data)
+    })
+
+// Stream some random data.
+createProgressiveRandomGenerator()
+    .setNumberOfPoints(250)
+    .generate()
+    .setStreamBatchSize(1)
+    .setStreamInterval(50)
+    .setStreamRepeat(false)
+    .toStream()
+    .forEach((point) => {
+        const y = point.y * (Math.random() * 20) - 10
+        line2.add({ x: point.x, y })
+        // check intesection of line series
+        getIntersection({ x: point.x, y })
+    })
+
+// Add On-Screen menu    
+chart.addOnScreenMenu(
+    [
+        [
+            // Default buttons
+            OnScreenMenuButtonType.ZoomInX,
+            OnScreenMenuButtonType.ZoomOutX,
+            OnScreenMenuButtonType.ZoomInY,
+            OnScreenMenuButtonType.ZoomOutY,
+            OnScreenMenuButtonType.ZoomToFit,
+            OnScreenMenuButtonType.ToggleAnimations,
+            // Custom button
+            {
+                icon: document.head.baseURI + 'examples/assets/lcjs_example_0706_onScreenMenu-icon.png',
+                dimensions: { rows: 1, columns: 1 },
+                opacity: '0.8',
+                color: 'blue',
+                shape: OnScreenMenuButtonShape.RoundedRectangle,
+                action: show,
+            },
+        ],
+    ],
+    OnScreenMenuButtonShape.RoundedRectangle,
+)
+
+// Function for actions of Custom On Screen Menu Button
+function show () {
+    if (!checked) {
+        point.dispose()
+    }else{
+        point.restore()
+    }
+    checked == false ? (checked = true) : (checked = false)
+}
+
+
+function getIntersection (currPoint) {
+    // Index of last generated point
+    const index = currPoint.x
+    if (currPoint.x > 0) {
+        // Check if lines were intersected
+        if (
+            (currPoint.y > arr1[index].y && prevPoint.y < arr1[index-1].y) ||
+            (currPoint.y < arr1[index].y && prevPoint.y > arr1[index-1].y)
+        ) {
+            // Find point of intersection
+            const interSectionPoint = calculateIntersection(
+                currPoint,
+                prevPoint,
+                arr1[index],
+                arr1[index-1],
+            )
+            point.add(interSectionPoint)
+        }
+        // Reassign curr point as prev. point
+        prevPoint = currPoint
+    }else{
+        // Assign curr point as prev. point
+        prevPoint = currPoint
+    }
+}
+
+
+// The explanation of formula can be found in the description below 
+function calculateIntersection(currPoint1, prevPoint1, currPoint2, prevPoint2) {
+    // Expressions of numerator
+    const exp1 = currPoint1.x * prevPoint1.y - currPoint1.y * prevPoint1.x 
+    const exp2 = currPoint2.x * prevPoint2.y - currPoint2.y * prevPoint2.x
+
+    const exp3 = currPoint2.x - prevPoint2.x 
+    const exp4 = currPoint1.x - prevPoint1.x 
+    const exp5 = currPoint2.y - prevPoint2.y 
+    const exp6 = currPoint1.y - prevPoint1.y 
+
+    // Denominator
+    const d1 = (currPoint1.x - prevPoint1.x) * (currPoint2.y - prevPoint2.y) 
+    const d2 = (currPoint1.y - prevPoint1.y) * (currPoint2.x - prevPoint2.x)
+    const d = d1 - d2
+
+    if (d === 0) {
+        throw new Error('Number of intersection points is zero or infinity.')
+    }
+    
+    const px = (exp1 * exp3 - exp4 * exp2) / d
+    const py = (exp1 * exp5 - exp6 * exp2) / d
+
+    const p = { x: px, y: py }
+
+    // Return point
+    return p
+}
